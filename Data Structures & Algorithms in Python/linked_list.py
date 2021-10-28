1126d560bbaee0061fd71b1aa5cac62852f377ff
Mobasshir Bhuiyan <mobasshirbhuiyan.shagor@gmail.com>
2021-10-29 00:30:02 +0600

    Update linked_list.py

--- Data Structures & Algorithms in Python/linked_list.py
+++ Data Structures & Algorithms in Python/linked_list.py
@@ -1,3 +1,13 @@
+"""The LinkedList code from before is provided below.
+Add three functions to the LinkedList.
+"get_position" returns the element at a certain position.
+The "insert" function will add an element to a particular
+spot in the list.
+"delete" will delete the first element with that
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
+
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -53,3 +63,35 @@ class LinkedList(object):
                 previous.next = current.next
             else:
                 self.head = current.next
+
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+
+# Test get_position
+# Should print 3
+print ll.head.next.next.value
+# Should also print 3
+print ll.get_position(3).value
+
+# Test insert
+ll.insert(e4,3)
+# Should print 4 now
+print ll.get_position(3).value
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print ll.get_position(1).value
+# Should print 4 now
+print ll.get_position(2).value
+# Should print 3 now
+print ll.get_position(3).value
